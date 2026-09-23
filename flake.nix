4d034fa2f8901b5032d751abf98979b2907d87f5
Arnaud <arnaud@status.im>
2026-09-23 14:53:08 +0400

    update package manager dependency

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-package-manager.url = "github:logos-co/logos-package-manager";
+    logos-package-manager.url = "github:logos-co/logos-package-manager?ref=feat/install-source";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
